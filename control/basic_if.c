75ef12e091a9471fcd6fc241789f16843f9c4c64
AraceliDanielaRamirezLopez <daniiela.ari33@gmail.com>
2020-11-02 17:31:19 -0600

    Ejemplo de la clase
    Estructura de selección simple

---
+++ control/basic_if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>// Cabecera 
+
+int main()// función principal 
+{
+	int A = 1;
+	if ( A > 0 )
+	{ 
+		printf ("El numero %d es positivo\n,", A);
+	}
+	else 
+	{
+		printf ("El numero %d es negativo\n,", A);
+	}
+	return 0;
+}
+
